2b2a98dd858610705f23a3407d65e23a5e18c1cf
minhduccse <1610800@hcmut.edu.vn>
2018-10-24 17:13:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/minhduc/spiandroidthings/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.minhduc.spiandroidthings;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.galarzaa.androidthings.Rc522;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+import com.google.android.things.pio.SpiDevice;
+
+import java.io.IOException;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class MainActivity extends Activity {
+    private static final String SPI_PORT = "SPI0.0";
+    private static final String PIN_RESET = "BCM25";
+    RfidTask mRfidTask;
+    String resultsText = "";
+    private Rc522 mRc522;
+    private TextView mTagDetectedView;
+    private TextView mTagUidView;
+    private TextView mTagResultsView;
+    private Button button;
+    private SpiDevice spiDevice;
+    private Gpio gpioReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTagDetectedView = (TextView) findViewById(R.id.tag_read);
+        mTagUidView = (TextView) findViewById(R.id.tag_uid);
+        mTagResultsView = (TextView) findViewById(R.id.tag_results);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRfidTask = new RfidTask(mRc522);
+                mRfidTask.execute();
+                ((Button) v).setText(R.string.reading);
+            }
+        });
+        PeripheralManager pioService = PeripheralManager.getInstance();
+        try {
+            spiDevice = pioService.openSpiDevice(SPI_PORT);
+            gpioReset = pioService.openGpio(PIN_RESET);
+            mRc522 = new Rc522(spiDevice, gpioReset);
+            mRc522.setDebugging(true);
+        } catch (IOException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if (spiDevice != null) {
+                spiDevice.close();
+            }
+            if (gpioReset != null) {
+                gpioReset.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class RfidTask extends AsyncTask<Object, Object, Boolean> {
+        private static final String TAG = "RfidTask";
+        private Rc522 rc522;
+
+        RfidTask(Rc522 rc522) {
+            this.rc522 = rc522;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            button.setEnabled(false);
+            mTagResultsView.setVisibility(View.GONE);
+            mTagDetectedView.setVisibility(View.GONE);
+            mTagUidView.setVisibility(View.GONE);
+            resultsText = "";
+        }
+
+        @Override
+        protected Boolean doInBackground(Object... params) {
+            rc522.stopCrypto();
+            while (true) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+                //Check if a RFID tag has been found
+                if (!rc522.request()) {
+                    continue;
+                }
+                //Check for collision errors
+                if (!rc522.antiCollisionDetect()) {
+                    continue;
+                }
+                byte[] uuid = rc522.getUid();
+                return rc522.selectTag(uuid);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (!success) {
+                mTagResultsView.setText(R.string.unknown_error);
+                return;
+            }
+            // Try to avoid doing any non RC522 operations until you're done communicating with it.
+            byte address = Rc522.getBlockAddress(2, 1);
+            // Mifare's card default key A and key B, the key may have been changed previously
+            byte[] key = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+            // Each sector holds 16 bytes
+            // Data that will be written to sector 2, block 1
+            byte[] newData = {0x0F, 0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00};
+            // In this case, Rc522.AUTH_A or Rc522.AUTH_B can be used
+            try {
+                //We need to authenticate the card, each sector can have a different key
+                boolean result = rc522.authenticateCard(Rc522.AUTH_A, address, key);
+                if (!result) {
+                    mTagResultsView.setText(R.string.authetication_error);
+                    return;
+                }
+                result = rc522.writeBlock(address, newData);
+                if (!result) {
+                    mTagResultsView.setText(R.string.write_error);
+                    return;
+                }
+                resultsText += "Sector written successfully";
+                byte[] buffer = new byte[16];
+                //Since we're still using the same block, we don't need to authenticate again
+                result = rc522.readBlock(address, buffer);
+                if (!result) {
+                    mTagResultsView.setText(R.string.read_error);
+                    return;
+                }
+                resultsText += "\nSector read successfully: " + Rc522.dataToHexString(buffer);
+                rc522.stopCrypto();
+                mTagResultsView.setText(resultsText);
+            } finally {
+                button.setEnabled(true);
+                button.setText(R.string.start);
+                mTagUidView.setText(getString(R.string.tag_uid, rc522.getUidString()));
+                mTagResultsView.setVisibility(View.VISIBLE);
+                mTagDetectedView.setVisibility(View.VISIBLE);
+                mTagUidView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+}
